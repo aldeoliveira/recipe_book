a07d24b20faba05c7a05860c8f3d990898096ae8
André <aldeoliveira@gmail.com>
2023-01-11 15:37:57 -0300

    fix: show only published recipes, fix category page title

--- recipes/views.py
+++ recipes/views.py
@@ -5,13 +5,15 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
+    # TODO: if the cover image is not big enough, there is a margin left on the card in this template
+    # TODO: change main logo text when in category as oposed to home
 
 
 def recipe(request, id):
