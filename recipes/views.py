8ae5fdd6321ed9c1898e4afb694473802538c90f
André <aldeoliveira@gmail.com>
2023-01-19 13:28:21 -0300

    fix: category page title, feat: category not found 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
 
 def home(request):
+    template = 'recipes/pages/home.html'
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
+    context = {'recipes': recipes}
+
+    return render(request, template, context)
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
+    template = 'recipes/pages/category.html'
+    queryset = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(queryset)
+    title = f'{recipes[0].category.name} - Category | '
+    context = {'recipes': recipes, 'title': title}
+
+    return render(request, template, context)
+
     # TODO: if the cover image is not big enough, there is a margin left on the card in this template
     # TODO: change main logo text when in category as oposed to home
 
